c2e662ae653e2065e8fd124b77e50fc24022669c
Sebastian Orozco Perez <sebastianorozcoperez2108@gmail.com>
2026-07-07 05:07:40 -0600

    Actualizar login

--- web/src/pages/Login.jsx
+++ web/src/pages/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
         e.preventDefault();
 
         try {
-            const response = await fetch('http://localhost:5000/api/login', {
+            const response = await fetch('https://the-citadel-api.onrender.com', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ correo, password })
