63ef63e37e9ede25296c8aba6742128c5a327b84
Sebastian Orozco Perez <sebastianorozcoperez2108@gmail.com>
2026-07-23 23:48:22 -0600

    Agregar imagenes de portadas y actualizar server.js

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use((req, res, next) => {
     console.log(`Recibí una petición: ${req.method} ${req.url}`);
     next();
